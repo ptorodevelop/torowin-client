ec4226aed07d383b83750ac723182ea4f3e0f18a
Duban Andrés Monsalve Suárez <duban.monsalve@onoff.com.co>
2026-04-27 00:34:18 -0500

    fix: use SSR mode for dynamic routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'sobre/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'legal/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
