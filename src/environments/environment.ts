ce577d42e335c414b37185bce84ad19de6365ce6
Duban Andrés Monsalve Suárez <duban.monsalve@onoff.com.co>
2026-04-27 00:24:45 -0500

    ci: add GitHub Actions deploy workflow

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // CONFIGURA TUS VARIABLES DE PRODUCCIÓN AQUÍ
   apiUrl: 'https://TU_DOMINIO_DE_PRODUCCION.com/api',
-  wsUrl: 'https://TU_DOMINIO_DE_PRODUCCION.com:8080', // Reverb luego
+  wsUrl: 'https://TU_DOMINIO_DE_PRODUCCION.com:8080',
 };
