7352179ffdfb9345d505c673ed15228c6fd02852
Duban Andrés Monsalve Suárez <duban.monsalve@onoff.com.co>
2026-04-27 00:39:08 -0500

    fix: use SSR server mode for all routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'sobre/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'legal/:slug',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
